733bd77fe7ae92ac9257655612fd268016d460a4
mozes32 <moussaebrahem32@gmail.com>
2022-10-30 22:50:40 +0200

    refactor: change svg to be dynamic width and height

--- src/modules/Splash/Splash.js
+++ src/modules/Splash/Splash.js
@@ -3,7 +3,7 @@ import { ReactComponent as BurgerIcon } from "../../assets/images/BurgerIcon.svg
 const Splash = () => {
   return (
     <div className="h-screen flex flex-col pt-[295px] items-center font-normal capitalize md:container">
-      <BurgerIcon />
+      <BurgerIcon width="115" height="115" />
       <h1 className="text-3xl text-white mt-3">
         fast <span className="text-primary">food</span>
       </h1>
