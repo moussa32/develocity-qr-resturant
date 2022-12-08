1028fa130528cf6185680e5ca0aedce812d0dceb
mozes32 <moussaebrahem32@gmail.com>
2022-12-08 13:33:41 +0200

    feat: added qr scanner page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Splash from "./modules/Splash/Splash";
 import Start from "./modules/Start/Start";
+import Scan from "./modules/Scan/Scan";
 import Home from "./modules/Home/Home";
 import FoodDetails from "./modules/FoodDetails/FoodDetails";
 
@@ -11,6 +12,7 @@ const App = () => {
       <main className="mainApp">
         <Routes>
           <Route index element={<Splash />} />
+          <Route path="scan" element={<Scan />} />
           <Route path="start" element={<Start />} />
           <Route path="home" element={<Home />} />
           <Route path=":foodId" element={<FoodDetails />} />
---
+++ src/modules/Scan/Scan.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { QrReader } from "react-qr-reader";
+
+const Scan = () => {
+  const [tableNumber, setTableNumber] = useState("No result");
+  return (
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setTableNumber(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        className="w-full"
+        style={{ width: "100%" }}
+      />
+      <p>{tableNumber}</p>
+    </>
+  );
+};
+
+export default Scan;
