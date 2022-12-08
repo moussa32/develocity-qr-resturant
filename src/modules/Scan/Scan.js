7375a068339b537c93f388b5a9ff0e7505daa48a
mozes32 <moussaebrahem32@gmail.com>
2022-12-08 14:34:32 +0200

    feat: scan qr code

--- src/modules/Scan/Scan.js
+++ src/modules/Scan/Scan.js
@@ -1,25 +1,42 @@
 import { useState } from "react";
 import { QrReader } from "react-qr-reader";
+import { useNavigate } from "react-router-dom";
+
+const RANDOM_TABLE_NUMBER = Math.floor(Math.random() * 100);
 
 const Scan = () => {
-  const [tableNumber, setTableNumber] = useState("No result");
+  const [tableNumber, setTableNumber] = useState("Didn't read the table number yet");
+  const [status, setStatus] = useState(null);
+  let navigate = useNavigate();
+
   return (
     <>
-      <QrReader
-        onResult={(result, error) => {
-          alert(result, error);
-          if (!!result) {
-            setTableNumber(result?.text);
-          }
+      {status === null && (
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setStatus({ type: "success", message: `Your table is ${RANDOM_TABLE_NUMBER}` });
+              setTableNumber(RANDOM_TABLE_NUMBER);
+              setTimeout(() => navigate("/start"), 3000);
+            }
 
-          if (!!error) {
-            console.info(error);
-          }
-        }}
-        className="w-full"
-        style={{ width: "100%" }}
-      />
-      <p>{tableNumber}</p>
+            if (!!error) {
+            }
+          }}
+          scanDelay={5000}
+          className="h-36"
+        />
+      )}
+
+      {status && (
+        <p
+          className={`${
+            status.type === "success" ? "text-green-500" : "text-red-500"
+          } text-center font-bold fs text-xl`}
+        >
+          {status.message}
+        </p>
+      )}
     </>
   );
 };
