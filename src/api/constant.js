5e0f7ae8689636aa6f9f756d0338dcb158d6c8f1
Moussa Ibrahim <moussaebrahem32@gmail.com>
2024-01-17 13:25:27 +0200

    refactor: change base URL

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const globalInstance = axios.create({
-  baseURL: "https://resturant.develocity.app/api/",
+  baseURL: "https://restaurant.develocity.link/api",
   headers: {
-    app_api_key: "jSxmLhazjtgaGLWYDNG%^&48MlTtfXOgn3qlcy!*XufXOg0@~n3qlcyJjsOfMoCs",
+    app_api_key:
+      "jSxmLhazjtgaGLWYDNG%^&48MlTtfXOgn3qlcy!*XufXOg0@~n3qlcyJjsOfMoCs",
   },
 });
